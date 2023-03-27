f1c36bba8a74c0b4a0e63e5517bac50d9d573e69
SHIVA-GARG <shivagarg.191ee150@nitk.edu.in>
2023-03-27 15:54:10 +0530

    preloader

--- main.js
+++ main.js
@@ -279,5 +279,9 @@ spinner.setAttribute("style","-webkit-transform: rotateY("+ angle +"deg); -moz-t
 }
 
 
+var loader = document.getElementById("preloader");
 
+window.addEventListener("load", function () {
+  loader.style.display = "none";
+});
 
