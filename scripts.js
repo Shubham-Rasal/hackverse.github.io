d8042bc8d6d9930e310cf8f3f699cfed0da34eb3
Saikiran Patil <patilrsaikiran@gmail.com>
2023-04-15 19:37:25 +0530

    updating the countdown timer, added discord link, some minor change in timeline

--- scripts.js
+++ scripts.js
@@ -43,7 +43,7 @@ navLinks.forEach((l) => {
 
 // *** Countdown logic Starts ***
 
-var date_str = new Date("2023-04-15T07:00:00.000Z");//.toLocaleString("en-US", { timeZone: "Asia/Kolkata" });
+var date_str = new Date("2023-04-16T06:30:00.000Z");//.toLocaleString("en-US", { timeZone: "Asia/Kolkata" });
 var countDownDate = Date.parse(date_str);
 
 // var utc = d.getTime() + (d.getTimezoneOffset() * 60000);
